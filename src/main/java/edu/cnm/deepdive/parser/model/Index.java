696b0849ac508f2057005817ea77bd01d922ac8a
Benjamin Quintana <bquintana52@ccnm.edu>
2021-02-26 09:31:33 -0800

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/parser/model/Index.java
@@ -0,0 +1,5 @@
+package edu.cnm.deepdive.parser.model;
+
+public class Index {
+
+}
---
+++ src/main/java/edu/cnm/deepdive/parser/model/View.java
@@ -0,0 +1,8 @@
+package edu.cnm.deepdive.parser.model;
+
+public class View {
+
+  //TODO Implement with appopriate fields, accessors, & mutators.
+
+
+}
